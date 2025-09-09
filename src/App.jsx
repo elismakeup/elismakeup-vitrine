b204d837a64ee0dc5d43eed808c4896679e1af4b
guiixta <ferreiragui@proton.me>
2025-09-09 12:39:45 -0300

    Trocando cores

--- src/App.jsx
+++ src/App.jsx
@@ -103,7 +103,7 @@ function App() {
 
 
       <main className='w-dvw min-h-dvh px-2 py-8 md:px-[5rem] md:py-[2rem] flex items-center justify-center bg-[url("/fundo.jpg")] bg-cover bg-center bg-no-repeat bg-fixed'>
-        <div className='bg-neutral-900 border-2 border border-fuschia-700 rounded-2xl p-4 sm:p-8 relative w-full max-w-4xl overflow-hidden'>
+        <div className='bg-neutral-900 border-2 border border-fuchsia-700 rounded-2xl p-4 sm:p-8 relative w-full max-w-4xl overflow-hidden'>
           
           <div 
             className="absolute z-0"
@@ -133,7 +133,7 @@ function App() {
 
           <div className='Header space-y-4 relative z-10'>
             <div className='Foto flex justify-center w-full'>
-              <img src="/perfil.jpg" className='w-[150px] h-[150px] rounded-full object-cover border-2 border-fuschia-700' alt="ElisMakeup" />   
+              <img src="/perfil.jpg" className='w-[150px] h-[150px] rounded-full object-cover border-2 border-fuchsia-700' alt="ElisMakeup" />   
             </div>
             <div className='Text flex flex-col justify-center items-center text-center space-y-2'>
               <span className='cursor-default text-white font-bold text-3xl tracking-wider'>ElisMakeup</span>
@@ -145,15 +145,15 @@ function App() {
               </div>
             </div>
             <div className='Social flex justify-center items-center py-2'>
-              <div className='p-2 rounded-lg flex justify-center items-center px-6 sm:px-8 gap-6 border border-gold'>
-                <a href="https://wa.me/556194030319/?text=Ol%C3%A1%20estou%20interessada(o)%20nos%20seus%20servi%C3%A7os!" target="_blank" rel="noopener noreferrer" className='cursor-pointer text-gold text-gold-H text-3xl transition-colors duration-300'><i className='bi bi-whatsapp'></i></a>
-                <a href="https://www.instagram.com/elissilvamakeup/" target="_blank" rel="noopener noreferrer" className='cursor-pointer text-gold text-3xl text-gold-H transition-colors duration-300'><i className='bi bi-instagram'></i></a>
+              <div className='p-2 rounded-lg flex justify-center items-center px-6 sm:px-8 gap-6 border border-fuchsia-700'>
+                <a href="https://wa.me/556194030319/?text=Ol%C3%A1%20estou%20interessada(o)%20nos%20seus%20servi%C3%A7os!" target="_blank" rel="noopener noreferrer" className='cursor-pointer text-fuchsia-700 hover:text-white text-3xl transition-colors duration-300'><i className='bi bi-whatsapp'></i></a>
+                <a href="https://www.instagram.com/elissilvamakeup/" target="_blank" rel="noopener noreferrer" className='cursor-pointer text-fuchsia-700 text-3xl hover:text-white transition-colors duration-300'><i className='bi bi-instagram'></i></a>
               </div>
             </div>
           </div>
           <div className='Body flex flex-col rounded-xl mt-6 relative z-10'>
             <div className='bg-fuchsia-700 p-3 rounded-t-lg text-center'>
-              <span className='font-bold text-black cursor-default text-xl'>Meus Serviços</span>
+              <span className='font-bold text-white cursor-default text-xl'>Meus Serviços</span>
             </div>
             <div className='bg-neutral-800 grid grid-cols-2 lg:grid-cols-3 rounded-b-lg p-2 md:p-4 gap-2 md:gap-4 max-h-96 overflow-y-auto overflow-x-hidden'>
             
